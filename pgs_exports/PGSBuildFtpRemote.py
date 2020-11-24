78ff0bc16448669809321fc06ea578474950914e
Laurent Gil <lg10@sanger.ac.uk>
2020-11-24 15:15:30 +0000

    Remove commented code

--- pgs_exports/PGSBuildFtpRemote.py
+++ pgs_exports/PGSBuildFtpRemote.py
@@ -11,15 +11,6 @@ class PGSBuildFtpRemote(PGSBuildFtp):
     ftp_root = 'ftp.ebi.ac.uk'
     ftp_path = 'pub/databases/spot/pgs/'
 
-    # def __init__(self, pgs_id, file_suffix ,type):
-    #     self.pgs_id = pgs_id
-    #     self.file_suffix = file_suffix
-    #     if type in self.allowed_types:
-    #         self.type = type
-    #     else:
-    #         print("The type '"+type+"' is not recognised!")
-    #         exit()
-
 
     def get_ftp_file(self,ftp_filename,new_filename):
         """ Download data file from the PGS FTP. """
@@ -45,7 +36,6 @@ class PGSBuildFtpRemote(PGSBuildFtp):
         ftp.quit()
 
 
-
     def get_ftp_md5_checksum(self):
         """ Get the MD5 of the Excel spreadsheet on FTP to compare with current Excel spreadsheet. """
 
@@ -64,43 +54,3 @@ class PGSBuildFtpRemote(PGSBuildFtp):
             return m.hexdigest()
         except:
             print("Can't find or access FTP file: "+self.ftp_root+'/'+filepath)
-
-
-    # def get_ftp_data_size(self):
-    #     """ Get the size of the Excel spreadsheet on FTP to compare with current Excel spreadsheet. """
-
-    #     ftp = FTP(self.ftp_root)     # connect to host, default port
-    #     ftp.login()                  # user anonymous, passwd anonymous@
-
-    #     filepath = self.ftp_path+self.data_dir+self.pgs_id+'/'
-    #     if self.type == 'metadata':
-    #         filepath += self.meta_dir+self.pgs_id+self.file_suffix
-    #     else:
-    #         filepath += self.scoring_dir+self.pgs_id+self.file_suffix
-
-    #     try:
-    #         size = ftp.size(filepath)
-    #         return size
-    #     except:
-    #         print("Can't find or access FTP file: "+self.ftp_root+'/'+filepath)
-
-
-    
-
-    # def get_md5_checksum(self,filename,blocksize=4096):
-    #     """ Returns MD5 checksum for the given file. """
-
-    #     md5 = hashlib.md5()
-    #     try:
-    #         file = open(filename, 'rb')
-    #         with file:
-    #             for block in iter(lambda: file.read(blocksize), b""):
-    #                 md5.update(block)
-    #     except IOError:
-    #         print('File \'' + filename + '\' not found!')
-    #         return None
-    #     except:
-    #         print("Error: the script couldn't generate a MD5 checksum for '" + filename + "'!")
-    #         return None
-
-    #     return md5.hexdigest()
